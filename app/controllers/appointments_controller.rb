9e2554cc8d01e657399a552b0254ccf281867980
Danilo Maccari <maccari78@gmail.com>
2025-03-17 17:08:43 -0300

    Implement Appointments CRUD OK

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,55 +1,54 @@
 class AppointmentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_appointment, only: [:show, :edit, :update, :destroy]
+  before_action :set_appointment, only: %i[show edit update destroy]
 
   def index
-    if current_user
-      @appointments = case current_user.role
-                      when "professional"
+    @appointments = if current_user
+                      case current_user.role
+                      when 'professional'
                         current_user.appointments_as_professional
-                      when "patient"
+                      when 'patient'
                         current_user.appointments_as_patient
                       else
                         Appointment.all # Para admin/secretary
                       end
-    else
-      @appointments = []
-    end
+                    else
+                      []
+                    end
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @appointment = Appointment.new
   end
 
+  def edit; end
+
   def create
     @appointment = Appointment.new(appointment_params)
-    @appointment.patient = current_user if current_user.role == "patient"
-    @appointment.professional = current_user.professional if current_user.role == "professional"
+    @appointment.patient = current_user if current_user.role == 'patient'
+    @appointment.professional = current_user.professional if current_user.role == 'professional'
 
     if @appointment.save
-      redirect_to appointments_path, notice: "Appointment was successfully created."
+      redirect_to appointments_path, notice: 'Appointment was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
-
   def update
     if @appointment.update(appointment_params)
-      redirect_to appointments_path, notice: "Appointment was successfully updated."
+      redirect_to appointments_path, notice: 'Appointment was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
+    @appointment = Appointment.find(params[:id])
     @appointment.destroy
-    redirect_to appointments_path, notice: "Appointment was successfully destroyed."
+    redirect_to appointments_path, notice: 'Appointment was successfully deleted.'
   end
 
   private
