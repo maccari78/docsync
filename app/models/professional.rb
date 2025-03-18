79caac61e1194c3dcf483d3a71153a3721b13a1c
Danilo Maccari <maccari78@gmail.com>
2025-03-17 22:40:41 -0300

    last fix

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,6 +1,8 @@
 class Professional < ApplicationRecord
   belongs_to :user, inverse_of: :professional
   has_many :appointments, dependent: :destroy, inverse_of: :professional
-  has_many :patients, foreign_key: :professional_id, dependent: :destroy # Nueva relación
-  enum specialty: { dentist: 0 }, _default: :dentist
+  has_many :patients, dependent: :destroy
+  enum :specialty, { dentist: 0 }, default: :dentist
+
+  delegate :email, to: :user, prefix: true
 end
