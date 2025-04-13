29ca827b9fe1076c6cd191c58bf948ef3c09a2a1
Danilo Maccari <maccari78@gmail.com>
2025-04-13 18:35:26 -0300

    add about me

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
@@ -29,6 +30,8 @@ Rails.application.routes.draw do
   post '/stripe/webhook', to: 'appointments#payment_notification'
 
   get '/debug/appointments', to: 'debug#appointments'
+
+  get 'about', to: 'static_pages#about'
 
   resources :admin_dashboards, only: [:index]
 
