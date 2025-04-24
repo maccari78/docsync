65bd7c74204630e60c820a68156d8445a420c2e6
Danilo Maccari <maccari78@gmail.com>
2025-04-24 15:11:02 -0300

    Remove channels/index.js after direct imports

--- public/assets/application-0ac5bcca0d01d53a708b060dc7144ab10eab5c9abf86c9516f007efd7bb9d43b.js
+++
@@ -1,7 +0,0 @@
-import "@hotwired/turbo-rails";
-import "controllers";
-import "channels";
-
-console.log("application.js: Script loaded at", new Date().toISOString());
-
-// No es necesario llamar a subscribeToChatChannel aquí, ya se llama en chat_channel.js;
