3402b3d6e4706dfd1a99951d6204d7b07e0be0f9
Danilo Maccari <maccari78@gmail.com>
2025-04-24 15:06:19 -0300

    Import consumer.js and chat_channel.js directly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 import "@hotwired/turbo-rails";
 import "controllers";
-import "channels";
+import "./channels/consumer";
+import "./channels/chat_channel";
 
 console.log("application.js: Script loaded at", new Date().toISOString());
-
-// No es necesario llamar a subscribeToChatChannel aquí, ya se llama en chat_channel.js
