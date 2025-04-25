788a69aa014759938d36f71e2e6c462feae89d3b
Danilo Maccari <maccari78@gmail.com>
2025-04-25 18:06:32 -0300

    upgrade 'consumer.js' to solve sync problem on production n' deploy

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,3 +1,5 @@
 import { createConsumer } from "@rails/actioncable";
 console.log("consumer.js: Loaded at", new Date().toISOString());
-export default createConsumer("wss://localhost:3000/cable");
+
+const cableUrl = document.querySelector('meta[name="action-cable-url"]')?.content || "ws://localhost:3000/cable";
+export default createConsumer(cableUrl);
