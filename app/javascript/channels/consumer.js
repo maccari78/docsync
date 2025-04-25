289c22467e3eb0eb105532d72e459e8a9d2eec5e
Danilo Maccari <maccari78@gmail.com>
2025-04-25 17:15:29 -0300

    Change 'consumer.js' to work in different environments

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,3 +1,10 @@
 import { createConsumer } from "@rails/actioncable";
 console.log("consumer.js: Loaded at", new Date().toISOString());
-export default createConsumer("wss://localhost:3000/cable");
+
+const cableUrl = process.env.NODE_ENV === "production" 
+  ? "wss://docsync-8ti1.onrender.com/cable" 
+  : window.location.protocol === "https:" 
+    ? `wss://${window.location.host}/cable` 
+    : `ws://${window.location.host}/cable`;
+
+export default createConsumer(cableUrl);
