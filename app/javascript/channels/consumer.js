8b84aebef4e54bc8c4c53273534e70863652d593
Danilo Maccari <maccari78@gmail.com>
2025-04-25 20:03:04 -0300

    upgrade 'consumer.js' to solve the syn problem on production or render

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,3 +1,15 @@
 import { createConsumer } from "@rails/actioncable";
 console.log("consumer.js: Loaded at", new Date().toISOString());
-export default createConsumer("wss://localhost:3000/cable");
+
+const host = window.location.host;
+let cableUrl;
+if (host.includes("docsync-8ti1.onrender.com")) {
+  cableUrl = "wss://docsync-8ti1.onrender.com/cable";
+} else if (host.includes("0.0.0.0:4000")) {
+  cableUrl = "ws://0.0.0.0:4000/cable";
+} else {
+  cableUrl = "wss://localhost:3000/cable";
+}
+
+console.log("consumer.js: Connecting to", cableUrl);
+export default createConsumer(cableUrl);
