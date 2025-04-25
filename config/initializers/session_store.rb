950c5fc4ac10be765039775fafadca9fb851af45
Danilo Maccari <maccari78@gmail.com>
2025-04-25 15:27:41 -0300

    Ensure session store secure false for local testing

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 Rails.application.config.session_store :cookie_store,
   key: '_docsync_session',
   # domain: :all,
-  secure: Rails.env.production?,
+  secure: false,
   same_site: :lax,
   httponly: true
 Rails.logger.info "Session store configured: key=#{Rails.application.config.session_options[:key]}, secure=#{Rails.application.config.session_options[:secure]}"
