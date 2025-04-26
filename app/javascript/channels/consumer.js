994759a97a4d2dfd192cb6c3f1f5a276466a019f
Danilo Maccari <maccari78@gmail.com>
2025-04-25 21:11:27 -0300

    upgrade to solve

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -2,13 +2,14 @@ import { createConsumer } from "@rails/actioncable";
 console.log("consumer.js: Loaded at", new Date().toISOString());
 
 const host = window.location.host;
+const protocol = window.location.protocol === "https:" ? "wss://" : "ws://";
 let cableUrl;
 if (host.includes("docsync-8ti1.onrender.com")) {
   cableUrl = "wss://docsync-8ti1.onrender.com/cable";
-} else if (host.includes("0.0.0.0:4000")) {
-  cableUrl = "ws://0.0.0.0:4000/cable";
+} else if (host.includes("0.0.0.0:4000") || host.includes("localhost:4000")) {
+  cableUrl = "ws://localhost:4000/cable";
 } else {
-  cableUrl = "ws://localhost:3000/cable";
+  cableUrl = protocol + "localhost:3000/cable";
 }
 
 console.log("consumer.js: Connecting to", cableUrl);
